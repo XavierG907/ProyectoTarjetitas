9f214be84b0c8fd1ab506435a5d7d400c80a11dc
IVAN ALEJANDRO ALVAREZ CHAVEZ <Ivan.AlvarezC@alumno.udg.mx>
2020-03-23 19:35:12 -0600

    Ivan editó menu principal 60%

--- Tarjetitas/MenuPrincipal.Designer.cs
+++ Tarjetitas/MenuPrincipal.Designer.cs
@@ -28,29 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
             this.panelMainMenu = new System.Windows.Forms.Panel();
+            this.buttonLogOut = new System.Windows.Forms.Button();
+            this.buttonConfig = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.label1Main = new System.Windows.Forms.Label();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panelMainMenu.SuspendLayout();
+            this.panelContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMainMenu
             // 
             this.panelMainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(110)))));
+            this.panelMainMenu.Controls.Add(this.label1Main);
+            this.panelMainMenu.Controls.Add(this.buttonHelp);
+            this.panelMainMenu.Controls.Add(this.buttonConfig);
+            this.panelMainMenu.Controls.Add(this.buttonLogOut);
             this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelMainMenu.Location = new System.Drawing.Point(0, 0);
             this.panelMainMenu.Name = "panelMainMenu";
-            this.panelMainMenu.Size = new System.Drawing.Size(893, 93);
+            this.panelMainMenu.Size = new System.Drawing.Size(831, 77);
             this.panelMainMenu.TabIndex = 0;
+            // 
+            // buttonLogOut
+            // 
+            this.buttonLogOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(110)))));
+            this.buttonLogOut.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLogOut.BackgroundImage")));
+            this.buttonLogOut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLogOut.FlatAppearance.BorderSize = 0;
+            this.buttonLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogOut.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonLogOut.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonLogOut.Location = new System.Drawing.Point(3, 3);
+            this.buttonLogOut.Name = "buttonLogOut";
+            this.buttonLogOut.Size = new System.Drawing.Size(50, 50);
+            this.buttonLogOut.TabIndex = 1;
+            this.buttonLogOut.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonLogOut.UseVisualStyleBackColor = false;
+            // 
+            // buttonConfig
+            // 
+            this.buttonConfig.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(110)))));
+            this.buttonConfig.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonConfig.BackgroundImage")));
+            this.buttonConfig.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonConfig.FlatAppearance.BorderSize = 0;
+            this.buttonConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConfig.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonConfig.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonConfig.Location = new System.Drawing.Point(66, 3);
+            this.buttonConfig.Name = "buttonConfig";
+            this.buttonConfig.Size = new System.Drawing.Size(50, 50);
+            this.buttonConfig.TabIndex = 3;
+            this.buttonConfig.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonConfig.UseVisualStyleBackColor = false;
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(75)))), ((int)(((byte)(110)))));
+            this.buttonHelp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonHelp.BackgroundImage")));
+            this.buttonHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonHelp.FlatAppearance.BorderSize = 0;
+            this.buttonHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHelp.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonHelp.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonHelp.Location = new System.Drawing.Point(129, 4);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(50, 50);
+            this.buttonHelp.TabIndex = 4;
+            this.buttonHelp.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonHelp.UseVisualStyleBackColor = false;
+            // 
+            // label1Main
+            // 
+            this.label1Main.AutoSize = true;
+            this.label1Main.BackColor = System.Drawing.Color.Transparent;
+            this.label1Main.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Main.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(135)))), ((int)(((byte)(143)))));
+            this.label1Main.Location = new System.Drawing.Point(255, 11);
+            this.label1Main.Name = "label1Main";
+            this.label1Main.Size = new System.Drawing.Size(315, 44);
+            this.label1Main.TabIndex = 5;
+            this.label1Main.Text = "MENU PRINCIPAL";
+            this.label1Main.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.button4);
+            this.panelContainer.Controls.Add(this.button3);
+            this.panelContainer.Controls.Add(this.button2);
+            this.panelContainer.Controls.Add(this.button1);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.ForeColor = System.Drawing.Color.Silver;
+            this.panelContainer.Location = new System.Drawing.Point(0, 77);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(831, 373);
+            this.panelContainer.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(94)))), ((int)(((byte)(109)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGray;
+            this.button1.Location = new System.Drawing.Point(235, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(355, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mis Barajas";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(94)))), ((int)(((byte)(109)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkGray;
+            this.button2.Location = new System.Drawing.Point(235, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Barajas Públicas";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(94)))), ((int)(((byte)(109)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkGray;
+            this.button3.Location = new System.Drawing.Point(235, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(355, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Barajas Favoritas";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(94)))), ((int)(((byte)(109)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkGray;
+            this.button4.Location = new System.Drawing.Point(235, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(355, 58);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Papelería de Reciclaje";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(34)))), ((int)(((byte)(49)))));
-            this.ClientSize = new System.Drawing.Size(893, 450);
+            this.ClientSize = new System.Drawing.Size(831, 450);
+            this.Controls.Add(this.panelContainer);
             this.Controls.Add(this.panelMainMenu);
             this.MaximizeBox = false;
             this.Name = "MenuPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TarjetitasPro V1.1";
+            this.panelMainMenu.ResumeLayout(false);
+            this.panelMainMenu.PerformLayout();
+            this.panelContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -58,6 +208,15 @@
         #endregion
 
         private System.Windows.Forms.Panel panelMainMenu;
+        private System.Windows.Forms.Button buttonLogOut;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Button buttonConfig;
+        private System.Windows.Forms.Label label1Main;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
